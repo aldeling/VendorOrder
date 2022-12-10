b4a7ada3ca2175d88b0a29c79f31ec7275623086
Athea DeLing <almdeling@gmail.com>
2022-12-10 12:30:27 -0500

    add boiler plate info to all files

---
+++ VendorOrder.Tests/Model.Tests/OrderTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.Testing;
+using System.Collections.Generic;
+using System;
+using VendorOrder.Models;
+
+namespace VendorOrder.Test
+{
+  [TestClass]
+  {
+
+    [TestMethod]
+    {
+      
+    }
+  }
+}
---
+++ VendorOrder/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendorOrder.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+  }
+}
